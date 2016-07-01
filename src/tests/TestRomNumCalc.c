e8de7ffc39c37e261d558a911d6c8c3dd5b10405
Kevin Hendrickson <hndrcksn@italcomputations.com>
2016-07-01 09:49:30 -0400

    Uncommenting first simple test broke the build based on the old Makefile. Had to add autotools to the mix. After much digging, searching, trial and error the project builds again. The unit test runs, but it passes and it is supposed to fail!

--- src/tests/TestRomNumCalc.c
+++ src/tests/TestRomNumCalc.c
@@ -1,16 +1,16 @@
 #include <check.h>
 #include "../lib/RomNumMath.h"
 
-//START_TEST (test_integer_addition)
-//{
-//    /* Simple test code to verify that check works as expected */
-//
-//    unsigned int num1, num2;
-//    num1 = 4;
-//    num2 = 5;
-//    ck_assert_int_eq(add(num1, num2), num1 + num2);
-//}
-//END_TEST
+START_TEST (test_integer_addition)
+{
+    /* Simple test code to verify that check works as expected */
+
+    unsigned int num1, num2;
+    num1 = 4;
+    num2 = 5;
+    ck_assert_int_eq(add(num1, num2), num1 + num2);
+}
+END_TEST
 
 int main(void)
 {
